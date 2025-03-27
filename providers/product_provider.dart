f1e44eabc0f358ac4328019e06785a359e8efbc0
Dheeraj531 <ballappallidheeraj17@gmail.com>
2025-03-27 23:13:23 +0530

    product_provider.dart

---
+++ providers/product_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductProvider extends ChangeNotifier {
+  List<Product> _products = [];
+
+  List<Product> get products => _products;
+
+  void addProduct(Product product) {
+    _products.add(product);
+    notifyListeners();
+  }
+}
+
